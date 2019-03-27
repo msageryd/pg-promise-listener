3707fc399f37a33077eceec73af5b9ce20120988
Michael Sageryd <michael@sageryd.se>
2019-03-27 13:24:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# pg-promise-listener
+
+Resilient notification listener for pg-promise
+
+The code in this package is mostly copied from Vitaly Tomilov's example code, which can be found here:
+https://github.com/vitaly-t/pg-promise/wiki/Robust-Listeners
+
+## Install
+
+```
+npm install github:msageryd/pg-promise-listener --save
+```
+
+## Use
+
+Example:
+
+```javascript
+const Listener = require('msageryd/pg-promise-listener');
+const pgp = require('pg-promise')();
+
+const myConnection = pgp({
+  host: 'localhost',
+  port: 5432,
+  database: 'myDatabase',
+  user: 'myUser',
+  password: 'myPassword',
+});
+
+function messageHandler(message) {
+  console.log(message);
+}
+
+const listener = new Listener({
+  dbConnection: myConnection,
+  onDatabaseNotification: messageHandler,
+  channel: 'myChannel',
+});
+```
---
+++ src/listener.js
@@ -0,0 +1,133 @@
+//Original inspiration:
+//https://github.com/vitaly-t/pg-promise/wiki/Robust-Listeners
+
+//Self-ping each second to check
+const PING_INTERVAL = 1000;
+
+//Any message with this string will be thrown away.
+//We are using a guid to ensure that we won't clash with an actual messages.
+const PING_MESSAGE = '0a7735a0-93b6-4830-835b-72d0f552381c';
+
+//Default: retry 10 times, with 5-second intervals
+const DEFAULT_RETRY_COUNT = 10;
+const DEFAULT_RETRY_INTERVAL = 5000;
+
+module.exports = DatabaseListener = function({
+  dbConnection, //pg-promise-connection to your database
+  onDatabaseNotification, //callback for message handling
+  channel, //name of your channel, i.e. the channel name with NOTIFY in your database
+  logger = null, //If you don't like console.log, insert your own logger
+  isParseJson = false, //Can your notify-messages be parsed from json?
+  maxRetryCount = 10,
+  retryInterval = 5000,
+}) {
+  if (!dbConnection) throw new Error('DatabaseListener: Missing dbConnection');
+  if (!channel) throw new Error('DatabaseListener: Missing channel name');
+
+  this.logger = logger || console.log;
+  this.db = dbConnection;
+  this.channel = channel;
+  this.onDatabaseNotification = onDatabaseNotification;
+  this.isParseJson = isParseJson;
+  this.maxRetryCount = maxRetryCount;
+  this.retryInterval = retryInterval;
+
+  // global connection for permanent event listeners
+  this.connection = null;
+
+  const onNotification = data => {
+    if (data.payload === PING_MESSAGE) return;
+
+    let message = data.payload;
+    if (this.isParseJson) {
+      try {
+        message = JSON.parse(data.payload);
+      } catch (e) {
+        this.logger(e);
+        this.logger(data);
+      }
+    }
+
+    if (this.onDatabaseNotification) {
+      this.onDatabaseNotification(message);
+    } else {
+      this.logger(message);
+    }
+  };
+
+  const setListeners = client => {
+    client.on('notification', onNotification);
+    return this.connection.none('LISTEN $1~', this.channel).catch(error => {
+      this.logger(error); // unlikely to happen
+    });
+  };
+
+  const removeListeners = client => {
+    client.removeListener('notification', onNotification);
+  };
+
+  const onConnectionLost = (err, e) => {
+    this.logger('Connectivity Problem:', err);
+    this.connection = null; // prevent use of the connection
+    removeListeners(e.client);
+    reconnect(this.retryInterval, this.maxRetryCount)
+      .then(() => {
+        this.logger('Successfully Reconnected');
+      })
+      .catch(() => {
+        this.logger('Connection Lost Permanently');
+        process.exit(); // exiting the process
+      });
+  };
+
+  const reconnect = (delay, maxAttempts) => {
+    delay = delay > 0 ? parseInt(delay) : 0;
+    maxAttempts = maxAttempts > 0 ? parseInt(maxAttempts) : 1;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        this.db
+          .connect({direct: true, onLost: onConnectionLost})
+          .then(obj => {
+            // global connection is now available
+            this.connection = obj;
+            resolve(obj);
+            return setListeners(obj.client);
+          })
+          .catch(error => {
+            this.logger('Error Connecting:', error);
+            if (--maxAttempts) {
+              reconnect(delay, maxAttempts)
+                .then(resolve)
+                .catch(reject);
+            } else {
+              reject(error);
+            }
+          });
+      }, delay);
+    });
+  };
+
+  const sendNotifications = () => {
+    //Initiate a "notify" from the database to check that we are connected
+    setInterval(() => {
+      if (this.connection) {
+        this.connection.none('NOTIFY $1~, $2', [this.channel, PING_MESSAGE]).catch(error => {
+          this.logger('Failed to Notify:', error); // unlikely to happen
+        });
+      }
+    }, PING_INTERVAL);
+  };
+
+  const init = () => {
+    return reconnect() // same as reconnect(0, 1)
+      .then(obj => {
+        this.logger('Successful Initial database Connection');
+        sendNotifications();
+      })
+      .catch(error => {
+        this.logger('Failed Initial database Connection:', error);
+      });
+  };
+
+  init();
+};
